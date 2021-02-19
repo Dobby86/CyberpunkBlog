07a336918e3c57f392a72c1ee9ba7387f992ae06
marco <dobermannpoker86@gmail.com>
2021-02-19 13:00:34 +0100

    fixed something

--- Cyberpunk2077/app/Http/Controllers/ProfileController.php
+++ Cyberpunk2077/app/Http/Controllers/ProfileController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\User;
-use Str;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
--- Cyberpunk2077/public/js/app.js
+++ Cyberpunk2077/public/js/app.js
@@ -49901,8 +49901,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Utente\Desktop\CyberpunkBlog\Cyberpunk2077\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Utente\Desktop\CyberpunkBlog\Cyberpunk2077\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! E:\ACQUISIZIONI\ProgettiPerAllenarsi\CyberpunkBlog\Cyberpunk2077\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! E:\ACQUISIZIONI\ProgettiPerAllenarsi\CyberpunkBlog\Cyberpunk2077\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
